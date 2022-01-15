db07f693bf76f3f7559a434fdf974fbfd96113ce
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-01-15 16:21:18 -0500

    added styling to table

--- public/app.js
+++ public/app.js
@@ -1,36 +1,35 @@
-
 const feedDisplay = document.getElementById('feed');
 
-
-
- fetch('http://localhost:2000/pen')
-.then( async response => {return response.json()})
-.then( async data => { 
-    
-
-    const parsedData = data['data']
+fetch('http://localhost:2000/data')
+  .then(async (response) => {
+    return response.json();
+  })
+  .then(async (data) => {
+    const parsedData = data['data'];
     // console.log(data['data'][0]['name'])
 
-    await parsedData.forEach( element => {
-        // console.log(element['name'])
-        const title = `${element.name}`;
-        console.log(title)
+    await parsedData.forEach((element) => {
+      let percentChange;
 
-        
-    
-    feedDisplay.insertAdjacentHTML('beforeend', title);
+      if (element['quote']['USD']['percent_change_24h'] >= 0) {
+        percentChange = `<td class='text-success'>${element['quote']['USD'][
+          'percent_change_24h'
+        ].toFixed(2)}%</td>`;
+      } else {
+        percentChange = `<td class='text-danger'>${element['quote']['USD'][
+          'percent_change_24h'
+        ].toFixed(2)}%</td>`;
+      }
 
+      const title = `<tr><td>${element['cmc_rank']} ${
+        element['name']
+      } <span class='text-secondary'>(${element['symbol']})</span</td>
+      <td>$${element['quote']['USD']['price'].toLocaleString('en-US')}</td>
+      ${percentChange} <td>${element['quote']['USD'][
+        'market_cap'
+      ].toLocaleString('en-US')}</td>
+      <td>${element['circulating_supply'].toLocaleString('en-US')}</td> </tr>`;
 
+      feedDisplay.insertAdjacentHTML('beforeend', title);
     });
-    
-    });
-
-
-
-    
-    
-    
-    
-
-
-
+  });
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest
 const qString = "?CMC_PRO_API_KEY=" + process.env.apiKey + "&start=1&limit=10&convert=USD";
 
 
-app.get('/pen',  async (req, res) => {
+app.get('/data',  async (req, res) => {
     const fetch_res = await fetch(url + qString);
     console.log( fetch_res)
     const json = await fetch_res.json();
@@ -23,8 +23,18 @@ fetch(url + qString).then(function(res){
   console.log(res);
 });
 
+const url2 = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?CMC_PRO_API_KEY=b73985c1-8612-4632-a4d5-8b6226ce9419&symbol=BTC'
 
+// app.get('/logo',  async (req, res) => {
+//   const fetch_res = await fetch(url + qString);
+//   console.log( fetch_res)
+//   const json = await fetch_res.json();
+//   res.json(json);
+// })
 
+// fetch(url + qString).then(function(res){
+// console.log(res);
+// });
 
 
 app.listen(PORT, () => {
